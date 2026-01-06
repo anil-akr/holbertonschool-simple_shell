1d55274337bb7e192597a1fe219d9f1b73ca860a
anil-akr <4nil000@protonmail.com>
2026-01-06 21:50:01 +0000

    Added parse_line.c

---
+++ parse_line.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+* parse_line - Split line into arguments
+* @line: Input line
+* Return: NULL-terminated array of strings
+*/
+
+char **parse_line(char *line)
+{
+    char **argv = NULL;
+    char *token;
+    int i = 0;
+
+    if (line == NULL)
+        return (NULL);
+
+    token = strtok(line, " \t\n");
+
+    while (token != NULL)
+    {
+        argv = realloc(argv, sizeof(char *) * (i + 2));
+        if (argv == NULL)
+            return (NULL);
+            
+        argv[i++] = token;
+        token = strtok(NULL, " \t\n");
+    }
+
+    if (argv != NULL)
+        argv[i] = NULL;
+
+    return (argv);
+}
