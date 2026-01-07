4d1bb5a94db736d0ed122a832124510c0080a484
anil-akr <4nil000@protonmail.com>
2026-01-07 14:08:46 +0000

    Fixed execute.c

--- execute.c
+++ execute.c
@@ -1,16 +1,19 @@
 #include "shell.h"
 
 /**
- * fork_and_execute - forks a child and executes a command
- * @command_path: full path to the command
- * @argv: array of arguments
- * Return: 0 on success, -1 on fork failure
+ * execute_command - executes a command exactly as typed
+ * @argv: null-terminated array of arguments
+ *
+ * Return: 0 always
  */
 
-static int fork_and_execute(char *command_path, char **argv)
+int execute_command(char **argv)
 {
 	pid_t pid;
 	int status;
+
+	if (argv == NULL || argv[0] == NULL)
+		return (0);
 
 	pid = fork();
 	if (pid == -1)
@@ -21,68 +24,14 @@ static int fork_and_execute(char *command_path, char **argv)
 
 	if (pid == 0)
 	{
-		if (execve(command_path, argv, environ) == -1)
-		{
-			perror("execve");
-			exit(EXIT_FAILURE);
-		}
+		execve(argv[0], argv, environ);
+		perror(argv[0]);
+		exit(EXIT_FAILURE);
 	}
 	else
-		wait(&status);
-
-	return (0);
-}
-
-/**
- * get_command_path - returns full path of a command
- * @argv: array of arguments
- *
- * Return: full path string if found, otherwise NULL
- */
-
-static char *get_command_path(char **argv)
-{
-	char *command_path;
-
-	if (argv[0][0] == '/')
-		command_path = argv[0];
-	else
-		command_path = find_path(argv[0]);
-
-	return (command_path);
-}
-
-/**
- * execute_command - executes a command with arguments
- * @argv: null-terminated array of arguments
- *
- * This function checks for built-ins, resolves the command path,
- * forks a child process and executes the command. Parent waits for child.
- *
- * Return: 0 on success, -1 on failure
- */
-
-int execute_command(char **argv)
-{
-	char *command_path;
-
-	if (!argv || !argv[0])
-		return (0);
-
-	if (built_in(argv, NULL) == 0) /* builtin executed */
-		return (0);
-
-	command_path = get_command_path(argv);
-	if (!command_path)
 	{
-		print_error(argv[0]);
-		return (-1);
+		wait(&status);
 	}
-
-	fork_and_execute(command_path, argv);
-
-	if (argv[0][0] != '/')
-		free(command_path);
 
 	return (0);
 }
