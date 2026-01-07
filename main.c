b1781b5852db24436393fe2001f6188686d77ef4
Milaa-au <mila.audu@icloud.com>
2026-01-07 14:02:14 +0000

    Correction main.c

--- main.c
+++ main.c
@@ -11,6 +11,19 @@
  * Return: 0 on successful termination
  */
 
+#include "shell.h"
+
+/**
+ * main - entry point of the simple shell
+ * Description: This function implements the main loop of the simple
+ * shell. It repeatedly displays a prompt, reads user input, parses
+ * the input into arguments, executes commands (builtin or external),
+ * and frees allocated memory. The loop continues until EOF (Ctrl+D)
+ * or an input error occurs.
+ *
+ * Return: 0 on successful termination
+ */
+
 int main(void)
 {
 	char *line;
@@ -35,3 +48,4 @@ int main(void)
 
 	return (0);
 }
+
