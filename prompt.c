37fd51a2d35756b0e4ed302e673155ac8c15b4a6
Milaa-au <mila.audu@icloud.com>
2026-01-07 09:22:31 +0000

    New verification that Betty is working properly

--- prompt.c
+++ prompt.c
@@ -4,6 +4,17 @@
 #include <unistd.h>
 #include <string.h>
 
+/**
+ * check_built_ins - checks if a string matches a builtin command
+ * Description: This function compares the given string with a list of
+ * supported builtin commands. If a match is found, it returns a pointer
+ * to the corresponding builtin function. If no match is found or if the
+ * string is NULL, the function returns NULL.
+ * @str: string containing the command name to check
+ *
+ * Return: pointer to the builtin function if found, or NULL otherwise
+ */
+
 void (*check_built_ins(char *str))(char *str)
 {
 	int i;
@@ -14,21 +25,31 @@ void (*check_built_ins(char *str))(char *str)
 		{NULL, NULL}
 	};
 
-    if (str == NULL)
-    {
-        return (NULL);
-    }
+	if (str == NULL)
+	{
+		return (NULL);
+	}
 
-    for (i = 0; buildin[i].name != NULL; i++)
-    {
-        if (strcmp(str, buildin[i].name) == 0)
-        {
-            return (buildin[i].func);
-        }
-    }
+	for (i = 0; buildin[i].name != NULL; i++)
+	{
+		if (strcmp(str, buildin[i].name) == 0)
+		{
+			return (buildin[i].func);
+		}
+	}
 
-    return (NULL);
+	return (NULL);
 }
+
+/**
+ * env_b - prints the current environment
+ * Description: This function displays all the environment variables
+ * available in the current process by writing each variable followed
+ * by a new line to the standard output.
+ * @line: unused parameter (original input line)
+ *
+ * Return: nothing
+ */
 
 void env_b(__attribute__((unused))char *line)
 {
@@ -44,6 +65,15 @@ void env_b(__attribute__((unused))char *line)
 		write(STDOUT_FILENO, "\n", 1);
 	}
 }
+
+/**
+ * exit_b - exits the shell
+ * Description: This function terminates the shell process by calling
+ * the exit system call with a status code of 0.
+ * @line: unused parameter (original input line)
+ *
+ * Return: nothing
+ */
 
 void exit_b(__attribute__((unused)) char *line)
 {
