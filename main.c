e8b6599d1920a26f5274dd72e804547291463172
Milaa-au <mila.audu@icloud.com>
2026-01-07 14:05:56 +0000

    Correction main.c & prompt.c

--- main.c
+++ main.c
@@ -11,19 +11,6 @@
  * Return: 0 on successful termination
  */
 
-#include "shell.h"
-
-/**
- * main - entry point of the simple shell
- * Description: This function implements the main loop of the simple
- * shell. It repeatedly displays a prompt, reads user input, parses
- * the input into arguments, executes commands (builtin or external),
- * and frees allocated memory. The loop continues until EOF (Ctrl+D)
- * or an input error occurs.
- *
- * Return: 0 on successful termination
- */
-
 int main(void)
 {
 	char *line;
@@ -31,19 +18,20 @@ int main(void)
 
 	while (1)
 	{
-		if (isatty(STDIN_FILENO))
-			write(STDOUT_FILENO, "$ ", 2);
+		display_prompt();
 
 		line = read_line();
 		if (!line)
-			break; 
+			break;
 
 		argv = parse_line(line);
 		if (argv)
+		{
 			execute_command(argv);
+			free(argv);
+		}
 
 		free(line);
-		free(argv);
 	}
 
 	return (0);
