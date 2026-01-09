85660ecd19c36d8fba0d6e649eafab3e93ab1c94
anil-akr <4nil000@protonmail.com>
2026-01-09 16:35:19 +0000

    Updated the header

--- shell.h
+++ shell.h
@@ -7,40 +7,35 @@
 #include <unistd.h>
 #include <sys/wait.h>
 #include <sys/types.h>
+#include <errno.h>
+
+extern char **environ;
 
 /**
- * struct builtin - represents a shell builtin command
- * Description: This structure associates the name of a builtin command
- * with a pointer to the function that executes it. It is used to identify
- * and execute built-in commands such as "exit" and "env".
- * @name: name of the builtin command
- * @func: pointer to the function that executes the builtin command
+ * struct builtin - builtin commands
+ * @name: name
+ * @func: function pointer
  */
-
 typedef struct builtin
 {
 	char *name;
 	void (*func)(char *);
 } builtin_t;
 
-extern char **environ;
-
+/* Prototypes */
 int built_in(char **command, char *line);
 void print_prompt(void);
-void (*check_built_ins(char *str))(char *str);
-void env_b(__attribute__((unused))char *line);
-void exit_b(__attribute__((unused)) char *line);
-
 int execute_command(char **argv);
 char *find_path(char *command);
-int handle_builtins(char **argv);
 void print_error(char *command);
+char **parse_line(char *line);
+char *read_line(void);
 
+/* Utils */
 int _strlen(char *s);
 char *_strcpy(char *dest, char *src);
 char *_strcat(char *dest, char *src);
-
-char **parse_line(char *line);
-char *read_line(void);
+int _strcmp(char *s1, char *s2);
+char *_strdup(char *str);
 
 #endif
