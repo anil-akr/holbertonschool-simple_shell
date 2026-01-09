46be10b28df6af4b515385c7f181e2ae8483f773
anil-akr <4nil000@protonmail.com>
2026-01-09 17:05:06 +0000

    Updated path.c

--- path.c
+++ path.c
@@ -1,89 +1,47 @@
 #include "shell.h"
 
 /**
- * get_path_from_environ - get PATH from environ manually
- * Return: PATH string or NULL if not found
- */
-
-char *get_path_from_environ(void)
-{
-	int i = 0;
-	char *prefix = "PATH=";
-
-	while (environ[i] != NULL)
-	{
-		if (strncmp(environ[i], prefix, 5) == 0)
-			return (environ[i] + 5);
-		i++;
-	}
-	return (NULL);
-}
-
-/**
- * build_full_path - concatenate dir + "/" + command
- * @dir: directory path
- * @command: command name
- * Return: malloced string or NULL
- */
-
-static char *build_full_path(char *dir, char *command)
-{
-	char *full_path;
-	int dir_len = _strlen(dir);
-	int cmd_len = _strlen(command);
-
-	full_path = malloc(dir_len + cmd_len + 2);
-	if (full_path == NULL)
-		return (NULL);
-
-	_strcpy(full_path, dir);
-	_strcat(full_path, "/");
-	_strcat(full_path, command);
-
-	return (full_path);
-}
-
-/**
- * find_path - searches PATH directories for a command
- * @command: name of the command
- * Return: malloced full path if found, NULL otherwise
+ * find_path - Searches for a command in the directories listed in PATH.
+ * @command: Name of the program (e.g. "ls").
+ * Return: Full path to the command (e.g. "/bin/ls"), or NULL if not found.
  */
 
 char *find_path(char *command)
 {
-	char *path_env, *path_copy, *dir, *full_path;
+    char *path_env, *path_copy, *dir, *full_path;
+    struct stat st;
 
-	if (command == NULL)
-		return (NULL);
+    if (command == NULL)
+        return (NULL);
 
-	path_env = get_path_from_environ();
-	if (path_env == NULL)
-		return (NULL);
+    if (stat(command, &st) == 0)
+        return (_strdup(command));
 
-	path_copy = strdup(path_env);
-	if (path_copy == NULL)
-		return (NULL);
+    path_env = _getenv("PATH");
+    if (path_env == NULL || _strlen(path_env) == 0)
+        return (NULL);
 
-	dir = strtok(path_copy, ":");
-	while (dir != NULL)
-	{
-		full_path = build_full_path(dir, command);
-		if (full_path == NULL)
-		{
-			free(path_copy);
-			return (NULL);
-		}
+    path_copy = _strdup(path_env);
+    
+    dir = strtok(path_copy, ":");
+    while (dir != NULL)
+    {
+        full_path = malloc(_strlen(dir) + _strlen(command) + 2);
+        if (full_path == NULL)
+            break;
 
-		if (access(full_path, X_OK) == 0)
-		{
-			free(path_copy);
-			return (full_path);
-		}
+        _strcpy(full_path, dir);
+        _strcat(full_path, "/");
+        _strcat(full_path, command);
 
-		free(full_path);
-		dir = strtok(NULL, ":");
-	}
-
-	free(path_copy);
-	return (NULL);
+        if (stat(full_path, &st) == 0)
+        {
+            free(path_copy);
+            return (full_path);
+        }
+        free(full_path);
+        dir = strtok(NULL, ":");
+    }
+    free(path_copy);
+    return (NULL);
 }
